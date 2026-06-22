e3e63223fff12677ee9ed1941581d8f249d85c0a
Famory_Josue_Sissoko <famoryjosuesissoko@gmail.com>
2026-06-22 10:23:11 +0000

    Sauvegarde de mes modifs notification et config

---
+++ src/main/java/groupe3/example/santekunafoniapp/Entity/Notification.java
@@ -0,0 +1,25 @@
+package groupe3.example.santekunafoniapp.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+    @Entity
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class Notification {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String titre;
+        private String message;
+        private LocalDateTime datePublication;
+        private boolean lue; // Pour gérer l'état
+
+        @ManyToOne
+        @JoinColumn(name = "utilisateur_id")
+        private Utilisateur utilisateur;
+    }
+
