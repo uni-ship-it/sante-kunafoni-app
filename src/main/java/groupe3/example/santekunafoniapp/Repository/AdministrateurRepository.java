3cf001ecba10e06de7cb9e518637941adefe58b1
Safiatou03 <safiatousamake030@gmail.com>
2026-06-22 12:24:55 +0000

    mes classes

---
+++ src/main/java/groupe3/example/santekunafoniapp/Repository/AdministrateurRepository.java
@@ -0,0 +1,6 @@
+package groupe3.example.santekunafoniapp.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdministrateurRepository extends JpaRepository {
+}
