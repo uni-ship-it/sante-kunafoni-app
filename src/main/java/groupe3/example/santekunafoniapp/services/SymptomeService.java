710c51b25b384bd402991ec2c0e1c5cf6aad3349
moulekafou <aissatamoulekafou11@gmail.com>
2026-06-22 09:17:04 +0000

    premierpush

---
+++ src/main/java/groupe3/example/santekunafoniapp/services/SymptomeService.java
@@ -0,0 +1,13 @@
+package groupe3.example.santekunafoniapp.services;
+
+import groupe3.example.santekunafoniapp.Repository.SymptomeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SymptomeService {
+    private final SymptomeRepository repository;
+
+    public SymptomeService(SymptomeRepository repository) {
+        this.repository = repository;
+    }
+}
