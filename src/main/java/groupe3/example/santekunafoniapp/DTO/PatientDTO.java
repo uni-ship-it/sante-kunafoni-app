228d0dbce9f311af765157c570bd65a1614bc09f
Hamathx999 <hamath.o.diallo18@gmail.com>
2026-07-03 08:54:00 +0000

    Ajout de la liaison patient_maladie

--- src/main/java/groupe3/example/santekunafoniapp/DTO/PatientDTO.java
+++ src/main/java/groupe3/example/santekunafoniapp/DTO/PatientDTO.java
@@ -1,11 +1,16 @@
 package groupe3.example.santekunafoniapp.DTO;
 
 import groupe3.example.santekunafoniapp.Entity.Role;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PatientDTO {
     private String nom;
     private String prenom;
@@ -16,4 +21,6 @@ public class PatientDTO {
     private String etat;
     private String sexe;
     private Date periode;
+
+    private List<Long> idMaladies;
 }
