8955dfb912c56a14660686973911170fe25c82ad
aissatamoulekafou11-cpu <aissatamoulekafou11@gmail.com>
2026-07-21 14:01:47 +0000

    fin-admin

--- src/main/java/groupe3/example/santekunafoniapp/services/serviceInterface/AdministrateurServiceInterface.java
+++ src/main/java/groupe3/example/santekunafoniapp/services/serviceInterface/AdministrateurServiceInterface.java
@@ -1,28 +1,25 @@
 package groupe3.example.santekunafoniapp.services.serviceInterface;
 
 
-import groupe3.example.santekunafoniapp.DTO.AdministrateurDTO;
-import groupe3.example.santekunafoniapp.Entity.AgentSante;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
+import groupe3.example.santekunafoniapp.DTO.AdministrateurRequestDTO;
+import groupe3.example.santekunafoniapp.DTO.AdministrateurResponseDTO;
 
 import java.util.List;
-import java.util.Optional;
 
 
 public interface AdministrateurServiceInterface {
 
 
-    public String ajouter(AdministrateurDTO dto);
+    public String ajouter(AdministrateurRequestDTO dto);
 
 
-    public String modifier(Long id, AdministrateurDTO dto);
+    public String modifier(Long id, AdministrateurRequestDTO dto);
 
 
-    AdministrateurDTO afficherParId(Long id);
+    AdministrateurResponseDTO afficherParId(Long id);
 
 
-    List<AdministrateurDTO> afficherTous();
+    List<AdministrateurResponseDTO> afficherTous();
 
 
     public String supprimer(Long id);
