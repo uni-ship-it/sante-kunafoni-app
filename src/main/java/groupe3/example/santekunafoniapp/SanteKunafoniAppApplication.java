24f31dc2e0841510bb758148690fe8478a138482
Famory_Josue_Sissoko <famoryjosuesissoko@gmail.com>
2026-07-29 10:53:25 +0000

    update

--- src/main/java/groupe3/example/santekunafoniapp/SanteKunafoniAppApplication.java
+++ src/main/java/groupe3/example/santekunafoniapp/SanteKunafoniAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling  // ✅ Active le système de planification automatique
+@EnableScheduling  //Active le système de planification automatique
 public class SanteKunafoniAppApplication {
 
     public static void main(String[] args) {
